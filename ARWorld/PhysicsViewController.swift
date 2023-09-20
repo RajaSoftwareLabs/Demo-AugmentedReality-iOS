f84ef8c788089ec8c349a1f55a8035e3f43a4dca
rsl-manmeetsingh <143382716+rsl-manmeetsingh@users.noreply.github.com>
2023-09-20 16:23:37 +0530

    Initial Code Commit (#1)

---
+++ ARWorld/PhysicsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PhysicsViewController.swift
+//  ARWorld
+//
+//  Created by Manmeet Singh on 11/09/23.
+//
+
+import UIKit
+import ARKit
+import RealityKit
+
+class PhysicsViewController: UIViewController {
+
+    var arView: ARView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        createARView()
+        
+    }
+    
+    private func createARView() {
+        arView = ARView(frame: .zero)
+        arView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(arView)
+        
+        NSLayoutConstraint.activate([
+            arView.topAnchor.constraint(equalTo: view.topAnchor),
+            arView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            arView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            arView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ])
+    }
+
+}
